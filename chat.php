05da8321a24eb440be870aa0c5d046a3e70e8498
princeearlnp-lab <princeearlnp@gmail.com>
2026-09-03 08:27:35 +0800

    Add files via upload

---
+++ chat.php
@@ -0,0 +1,74 @@
+<?php
+?>
+
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Group Chat - Study First</title>
+
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 20px;
+            font-size: 16px;
+            background: rgb(86, 136, 96);
+            color: #cfbcee;
+        }
+
+        nav {
+            background-color: #333;
+            padding: 10px;
+        }
+
+        nav ul {
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+        }
+
+        nav li {
+            display: inline;
+            margin-right: 20px;
+        }
+
+        nav a {
+            color: white;
+            text-decoration: none;
+        }
+
+        nav a:hover {
+            color: lightblue;
+        }
+    </style>
+
+</head>
+
+<body>
+
+    <h1>STUDY FIRST!</h1>
+    <h3> Group Chat</h3>
+
+    <hr>
+
+    <nav>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="notes.php">Notes</a></li>
+            <li><a href="quizzes.php">Quizzes</a></li>
+            <li><a href="chat.php"> Group Chat</a></li>
+        </ul>
+    </nav>
+
+    <br>
+
+    <h2>Study First-Group Chat</h2>
+    <p>Welcome to the chat page!</p>
+
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,70 @@
+<?php
+?>
+
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>SG-WEBSITE PROTOTYPE 1.00</title>
+
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 20px;
+            font-size: 16px;
+            background: cyan;
+        }
+
+        nav {
+            background-color: #333;
+            padding: 10px;
+        }
+
+        nav ul {
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+        }
+
+        nav li {
+            display: inline;
+            margin-right: 20px;
+        }
+
+        nav a {
+            color: white;
+            text-decoration: none;
+        }
+
+        nav a:hover {
+            color: lightblue;
+        }
+    </style>
+
+</head>
+
+<body>
+
+    <h1>STUDY FIRST!</h1>
+
+    <h3>Welcome to the Study First Website</h3>
+
+    <hr>
+    <br>
+
+    <nav>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="notes.php">Notes</a></li>
+            <li><a href="quizzes.php">Quizzes</a></li>
+            <li><a href="chat.php"> Group Chat</a></li>
+        </ul>
+    </nav>
+
+</body>
+
+</html>
