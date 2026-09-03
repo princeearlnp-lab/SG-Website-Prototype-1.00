81f604d6d9cdb4e09bc138bacc1d252bc92b5abd
princeearlnp-lab <princeearlnp@gmail.com>
2026-09-03 15:28:47 +0800

    Create db.php

---
+++ db.php
@@ -0,0 +1,13 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$password = "";
+$database = "login credentials";
+
+$conn = new mysqli($host, $user, $password, $database);
+
+if ($conn->connect_error) {
+    die("Database connection failed: " . $conn->connect_error);
+}
+?>
