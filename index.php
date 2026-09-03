86e0b6cf5a351bec8a8d26ae74205a5154eaf35f
princeearlnp-lab <princeearlnp@gmail.com>
2026-09-03 09:16:59 +0800

    Update index.php

--- index.php
+++ index.php
@@ -16,7 +16,12 @@
             font-family: Arial, sans-serif;
             margin: 20px;
             font-size: 16px;
-            background: cyan;
+            background: linear-gradient(#e00404, #e0044a, #e0047d, #e004ad, #e004d9);
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+            background-size: cover;
+            text-align: center;
+
         }
 
         nav {
@@ -51,10 +56,26 @@
 
     <h1>STUDY FIRST!</h1>
 
-    <h3>Welcome to the Study First Website</h3>
+    <marquee
+        behavior="srolling"
+        direction="left"
+        scrollamount="10"
+        onmouseover="this.stop()"
+        onmouseout="this.start()"
+        style="
+    color: #d78d8d;
+    font-size: 20px;
+    font-weight: bold;
+    text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
+    font-family:'courier new' , monospace;
+    "><b>WELCOME TO OUR WEBSITE!!</b></marquee>
+
+
 
     <hr>
     <br>
+
+
 
     <nav>
         <ul>
@@ -65,6 +86,13 @@
         </ul>
     </nav>
 
+
+
 </body>
 
 </html>
+
+/*notes for next time:
+1. finish the homepage design
+2. follow up on the other pages
+3. add more content on other pages */
